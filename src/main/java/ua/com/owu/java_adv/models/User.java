da203dd4a4d10b06b917cf70784b09054d5f6caf
plokhykh <mplokhykh@gmail.com>
2022-03-24 23:21:05 +0200

    lesson2

--- src/main/java/ua/com/owu/java_adv/models/User.java
+++ src/main/java/ua/com/owu/java_adv/models/User.java
@@ -1,14 +1,23 @@
 package ua.com.owu.java_adv.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
-@AllArgsConstructor
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @NoArgsConstructor
+@Getter
+@ToString
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
-    private boolean status;
+    private int age;
+
+    public void setId(int id) {
+    }
 }
